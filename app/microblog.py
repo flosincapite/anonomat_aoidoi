a36beaefa9e20ba412ba375c91a3081f810d1d67
Cory <cory.massaro@gmail.com>
2018-07-25 01:14:14 -0700

    Added config with secret_key for wtforms.

--- app/microblog.py
+++ app/microblog.py
@@ -2,4 +2,4 @@ from app import wsgi_app
 
 
 if __name__ == '__main__':
-  wsgi_app.run()
+  wsgi_app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,56 @@
-import app
 import flask
+
+import app
+from app import blog_post
+from app import submit
+
+
+"""
+Sidebar:
+  - home
+  - issues
+  - submit
+  - blog
+
+ISSUE:
+  - embedded multi-page .pdf viewer
+
+SUBMIT:
+  - upload & email form
+
+BLOG:
+  - displays posts by select writers
+"""
 
 
 @app.wsgi_app.route('/')
 @app.wsgi_app.route('/index')
 def index():
-  return flask.render_template('index.html')
+  return flask.render_template(
+      'index.html', title='Anonomat Aoidoi', page_title='Anonomat Aoidoi')
 
 
 @app.wsgi_app.route('/pdf')
 def pdf():
-  return flask.render_template('view_pdf.html', pdf_file='static/test.pdf')
+  return flask.render_template(
+      'view_pdf.html', pdf_file='static/test.pdf', page_title='Issue 1')
+
+
+@app.wsgi_app.route('/blog')
+def blog():
+  blog_posts = [
+      blog_post.BlogPost(
+        author='Juvenalis', date='then', title='Custodes?',
+        cdata='quis custodiet ipsos/'),
+      blog_post.BlogPost(
+        author='Valmiki', date='then', title='Birds',
+        cdata='kuujantam raama raameti'),
+  ] 
+  return flask.render_template(
+      'blog.html', blog_posts=blog_posts, page_title='Blog')
+
+
+@app.wsgi_app.route('/submit')
+def submit():
+  return flask.render_template(
+      'submit.html', page_title='Submit', submit_form=submit.SubmitForm())
