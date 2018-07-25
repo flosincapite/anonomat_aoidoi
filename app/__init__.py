063d84b7bef07294e33197469e6a484578b58d07
Cory <cory.massaro@gmail.com>
2018-07-25 01:14:53 -0700

    Config's SECRET_KEY is loaded in __init__.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 import flask
-import flask_bootstrap
+# import flask_bootstrap
+
+from app import config 
+
 
 wsgi_app = flask.Flask(__name__)
-bootstrap = flask_bootstrap.Bootstrap(wsgi_app)
+wsgi_app.config.from_object(config.Config)
+# bootstrap = flask_bootstrap.Bootstrap(wsgi_app)
 
 from app import routes
