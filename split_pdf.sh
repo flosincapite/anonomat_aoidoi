6642f248c633b73adb346ead9e697ea1d018abe0
Cory <cory.massaro@gmail.com>
2018-09-17 11:35:59 -0700

    Added new issue.

---
+++ split_pdf.sh
@@ -0,0 +1,20 @@
+if [[ ${1} == "" ]]; then
+  echo "Usage: split_pdf.sh <original_file> <num_pages> <output_directory>"
+  exit 1
+fi
+if [[ ${2} == "" ]]; then
+  echo "Usage: split_pdf.sh <original_file> <num_pages> <output_directory>"
+  exit 1
+fi
+if [[ ${3} == "" ]]; then
+  echo "Usage: split_pdf.sh <original_file> <num_pages> <output_directory>"
+  exit 1
+fi
+
+FILENAME=${1}
+LASTPAGE=${2}
+OUTDIR=${3}
+
+for i in $(eval echo {1..${LASTPAGE}}); do
+  pdftk ${FILENAME} cat ${i} output ${OUTDIR}/${i}.pdf
+done
