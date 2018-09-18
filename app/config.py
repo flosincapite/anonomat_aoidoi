e9aa57497528ff198df2a3cd1c4b07b4079956d6
Cory <cory.massaro@gmail.com>
2018-09-18 13:15:38 -0700

    Before converting to .png.

--- app/config.py
+++ app/config.py
@@ -1,8 +1,15 @@
 import os
 
 
+_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+def _rooted(location):
+  return os.path.join(_ROOT, location)
+
+
 class Config(object):
   SECRET_KEY = os.environ.get('ANONOMAT_KEY') or 'anonomat_aoidoi'
-  DATABASE_DIRECTORY = '/home/cory/projects/online_journal/app/static/issues'
-  BLOG_DIRECTORY = '/home/cory/projects/online_journal/app/static/blog_posts'
-  USER_DIRECTORY = '/home/cory/projects/online_journal/data/users'
+  DATABASE_DIRECTORY = _rooted('static/issues')
+  BLOG_DIRECTORY = _rooted('static/blog_posts')
+  USER_DIRECTORY = _rooted('data/users')
---
+++ zip_it.sh
@@ -0,0 +1,3 @@
+zip -r aoidoi.zip /home/cory/projects/online_journal/ \
+  -x \*online_journal/.git\* -x \*online_journal/.git\* \
+  -x \*online_journal/venv/\* -x \*online_journal/*.sh\*
